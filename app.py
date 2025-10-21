9f71201f2599c007967c72cef61be6c9b147dfd9
Victor Campos Leal <victor.campos.ext@sibelco.com>
2025-10-21 12:34:38 +0200

    Fix: bind to 0.0.0.0 and dynamic port forRender

--- app.py
+++ app.py
@@ -41,6 +41,6 @@ def adyen_webhook():
     return jsonify({"status": "[accepted]"}), 200
 
 if __name__ == '__main__':
-    import os
+    import os 
 port = int(os.environ.get("PORT", 5000))
 app.run(host="0.0.0.0", port=port)
